b60f684e18335736351dac85b58e756123e87d07
Charles1104 <charles.xavier.verleyen@gmail.com>
2017-03-24 11:35:43 -1000

    All tests passed

--- challenge_6.js
+++ challenge_6.js
@@ -6,11 +6,32 @@
  */
 exports.SumOfASquare = SumOfASquare;
 
-function SumOfASquare(naturalNumbers){
+var  SumOfASquare = function(naturalNumbers){
   this.naturalNumbers = naturalNumbers;
-}
+};
 
 // do work here
 
 // overwrite the object prototype, or add methods to the prototype
 // to satisfy the two method calls, sumOfSquares() and squareOfTheSums()
+
+SumOfASquare.prototype.sumOfSquares = function(){
+  var sum = 0;
+  for(var i=1; i <= this.naturalNumbers; i++){
+    sum += Math.pow(i, 2);
+  }
+  return sum;
+};
+
+
+SumOfASquare.prototype.squareOfTheSums = function(){
+  var sum = 0;
+  for(var i=1; i <= this.naturalNumbers; i++){
+    sum += i;
+  }
+  return Math.pow(sum, 2);
+};
+
+var test = new SumOfASquare(5);
+console.log(test.sumOfSquares());
+console.log(test.squareOfTheSums());
